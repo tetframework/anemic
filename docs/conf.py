84a84cab04255fa1cac2b25f0a63136cf11dbb0b
Antti Haapala <antti.haapala@interjektio.fi>
2024-01-04 20:16:36 +0200

    better docs?

--- docs/conf.py
+++ docs/conf.py
@@ -1,11 +1,22 @@
-# sphinx configuration file
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 import os
 import sys
 
-html_theme = "sphinx_rtd_theme"
-
 # -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "Anemic Framework"
+copyright = "2024, Interjektio Oy"
+author = "Interjektio Oy"
+release = "0.0.1"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.doctest",
@@ -13,5 +24,17 @@ extensions = [
     "sphinx.ext.viewcode",
     "sphinx_copybutton",
 ]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_static_path = ["_static"]
+html_theme = "sphinx_rtd_theme"
+
+# -- Define import paths for autodoc -----------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html#module-sphinx.ext.autodoc
 
 sys.path.insert(0, os.path.abspath("../src"))
